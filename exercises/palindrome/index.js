710b9a4a32f964ef2180b2948bae01db663447bd
ahmedacqua <ahmedacqua@gmail.com>
2020-11-22 20:09:13 +0530

    palidndrome-reversestring

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str){
+    return str.split('').every((char,index)=>{
+        return char === str[(str.length -1) - index];
+    });
+}
 
+
+/*function palindrome(str) {
+    let reversed =  str.split('').reverse().join('');
+    return reversed === str;
+}*/
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,8 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-    return str.split('').reverse().join('');
+function reverse(str){
+    debugger;
+    return str.split('').reduce((rev,char) => char + rev, "")
 }
 
 module.exports = reverse;
+
+/*function reverse(str) {
+    return str.split('').reverse().join('');
+}*/
+
+/*function reverse(str) {
+    let reversestring = "";
+    for(let character of str){
+    reversestring = character + reversestring;
+    }
+    return reversestring;
+}*/
